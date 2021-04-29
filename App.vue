107f65dd122b9ee706a36911a61873155e0ec30f
varshil241296 <81026407+varshil241296@users.noreply.github.com>
2021-04-29 19:04:22 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <date-picker v-model="date" lang="en" type="date"/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+    //DatePicker
+  },
+  // data() {
+  //   return {
+  //     date: new Date(),
+  //   };
+  // }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
